f00192688507f6a6251770b9bb83702697727bcf
RyeNguyen <minhiq149@gmail.com>
2022-07-04 11:03:41 +0700

    Solve the contrast color problem of the ColoursSection

--- src/layouts/Colours.component.jsx
+++ src/layouts/Colours.component.jsx
@@ -1,54 +1,61 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-import {variables} from "../components/styles/GlobalVariables";
-import {BodyMain, H3} from "../components/styles/TextStyles";
-import {ColoursData} from "../data/ColoursData";
-import {colorTheme} from "../components/styles/ColorStyles";
+import { variables } from "../components/styles/GlobalVariables";
+import { BodyMain, H3 } from "../components/styles/TextStyles";
+import { ColoursData } from "../data/ColoursData";
+import { colorTheme } from "../components/styles/ColorStyles";
 
 import Button from "../components/Button.component";
 
-const ColorContrastCalc = require("color-contrast-calc").ColorContrastCalc;
+const ContrastChecker = require("color-contrast-calc").ContrastChecker;
 
 const Colours = () => {
-    const [activeColour, setActiveColour] = useState(1);
+  const [activeColour, setActiveColour] = useState(1);
 
-    const calculateTextColor = (bgColor) => {
-        const text = ColorContrastCalc.colorFrom(colorTheme.secondary);
-        const background = ColorContrastCalc.colorFrom(bgColor).withGrayscale();
-        const aaaColor = text.findBrightnessThreshold(background, 'AAA');
-        console.log(text, background, text.contrastRatioAgainst(background));
-        return aaaColor.hexCode;
-    }
+  const calculateTextColor = (bgColor) => {
+    const ratio = ContrastChecker.contrastRatio(colorTheme.text, bgColor);
+    return ContrastChecker.ratioToLevel(ratio) === "A" ||
+      ContrastChecker.ratioToLevel(ratio) === "-" ||
+      ContrastChecker.ratioToLevel(ratio) === "AA"
+      ? colorTheme.secondary
+      : colorTheme.text;
+  };
 
-    return (
-        <ColoursSection>
-            <ColoursHeader className="layout--flex">
-                <ColoursHeaderTitle>Popular Colours</ColoursHeaderTitle>
-                <ColoursHeaderLink>View all</ColoursHeaderLink>
-            </ColoursHeader>
-            <ColoursList>
-                {ColoursData.map((colour) => (
-                    <ColourWrapper
-                        key={colour.id}
-                        backgroundColor={colour.color}
-                        textColor={() => calculateTextColor(colour.color)}
-                        onClick={() => setActiveColour(colour.id)}
-                        className={activeColour === colour.id ? 'colour--active' : ''}
-                    >
-                        <ColourNameWrapper>
-                            <ColourId>{colour.id}</ColourId>
-                            <ColourName className='colour__name'>{colour.name}</ColourName>
-                        </ColourNameWrapper>
-                        <ColourButtons className='button__list'>
-                            <Button color={() => calculateTextColor(colour.color)} title={`${colour.name} Test Pot - $${colour.price}`}/>
-                            <Button color={() => calculateTextColor(colour.color)} title='All Colours'/>
-                        </ColourButtons>
-                    </ColourWrapper>
-                ))}
-            </ColoursList>
-        </ColoursSection>
-    );
+  return (
+    <ColoursSection>
+      <ColoursHeader className="layout--flex">
+        <ColoursHeaderTitle>Popular Colours</ColoursHeaderTitle>
+        <ColoursHeaderLink>View all</ColoursHeaderLink>
+      </ColoursHeader>
+      <ColoursList>
+        {ColoursData.map((colour) => (
+          <ColourWrapper
+            key={colour.id}
+            backgroundColor={colour.color}
+            textColor={() => calculateTextColor(colour.color)}
+            onClick={() => setActiveColour(colour.id)}
+            className={activeColour === colour.id ? "colour--active" : ""}
+          >
+            <ColourNameWrapper>
+              <ColourId>{colour.id}</ColourId>
+              <ColourName className="colour__name">{colour.name}</ColourName>
+            </ColourNameWrapper>
+            <ColourButtons className="button__list">
+              <Button
+                color={calculateTextColor(colour.color)}
+                title={`${colour.name} Test Pot - $${colour.price}`}
+              />
+              <Button
+                color={calculateTextColor(colour.color)}
+                title="All Colours"
+              />
+            </ColourButtons>
+          </ColourWrapper>
+        ))}
+      </ColoursList>
+    </ColoursSection>
+  );
 };
 
 const ColoursSection = styled.div``;
@@ -70,8 +77,8 @@ const ColoursList = styled.div`
 const ColourWrapper = styled.div`
   flex: 0.5;
   height: 100%;
-  background-color: ${props => props.backgroundColor};
-  color: ${props => props.textColor};
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.textColor};
   padding-top: ${variables.sizeExtraLarge};
   display: flex;
   position: relative;
